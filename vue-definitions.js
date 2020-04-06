063a7eb7fc463e8837e14138fa4a5d38f65a2a38
Arjo <arjo129@gmail.com>
2020-04-06 09:01:17 +0800

    Added watch+debounce

--- vue-definitions.js
+++ vue-definitions.js
@@ -406,9 +406,29 @@ let app = new Vue({
         this.autoplay = false; // disable autoplay on first play
       }
     },
+
+    searchField() {
+      let debouncedSearch = this.debounce(this.search, 250, false);
+      debouncedSearch();
+    }
   },
 
   methods: {
+
+    debounce(func, wait, immediate) { //https://davidwalsh.name/javascript-debounce-function
+      var timeout;
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    },
 
     myMax() { //https://stackoverflow.com/a/12957522
       var par = []
